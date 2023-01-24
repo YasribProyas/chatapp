c958e6a5f9cd50bc2035d9838a90aea38794be8b
Proyas <37238596+Proyas21@users.noreply.github.com>
2023-01-24 21:08:49 +0600

    removed AppLoginCheck

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,14 +11,14 @@ import { AuthContextProvider } from "./contexts/AuthContext";
 function App() {
   return (
     <AuthContextProvider>
-      <AppLoginCheck>
-        <Routes>
-          <Route path="/" element={<Navigate to="chat" />} />
-          <Route path="chat/*" element={<Chat />} />
-          <Route path="login" element={<LoginForm />} />
-          <Route path="signup" element={<SignupForm />} />
-        </Routes>
-      </AppLoginCheck>
+      {/* <AppLoginCheck> */}
+      <Routes>
+        <Route path="/" element={<Navigate to="chat" />} />
+        <Route path="chat/*" element={<Chat />} />
+        <Route path="login" element={<LoginForm />} />
+        <Route path="signup" element={<SignupForm />} />
+      </Routes>
+      {/* </AppLoginCheck> */}
     </AuthContextProvider>
   );
 }
