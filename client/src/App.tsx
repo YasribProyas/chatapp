de95406f8f698029e379c8feafc3f5572e433779
Proyas <37238596+Proyas21@users.noreply.github.com>
2023-01-24 22:48:28 +0600

    bringing back AppLoginCheck

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,17 +12,17 @@ import { AuthContextProvider } from "./contexts/AuthContext";
 function App() {
   return (
     <AuthContextProvider>
-      {/* <AppLoginCheck> */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navigate to="chat" />} />
-          <Route path="/chat/*" element={<Chat />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-          <Route path="*" element={<h1>Not found</h1>} />
-        </Routes>
-      </BrowserRouter>
-      {/* </AppLoginCheck> */}
+      <AppLoginCheck>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Navigate to="chat" />} />
+            <Route path="/chat/*" element={<Chat />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/signup" element={<SignupForm />} />
+            <Route path="*" element={<h1>Not found</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </AppLoginCheck>
     </AuthContextProvider>
   );
 }
